95505b35a05209d23379c7bae8e86539218823aa
alex182 <format255@bk.ru>
2017-10-13 18:05:36 +0300

    4

--- js/components/plugins/dotdotdot-init.js
+++ js/components/plugins/dotdotdot-init.js
@@ -1,32 +1,34 @@
 require('../../vendor/jquery.dotdotdot.js');
 
 $('.dotdotdot').dotdotdot({
-    wrap: 'word',
-    watch: true,
-    tolerance: 0
+   wrap: 'word',
+   watch: true,
+   tolerance: 0
 });
 
 $('.info-cut').dotdotdot();
 $('.info-cut').trigger('isTruncated', function( isTruncated ) {
-    if ( isTruncated ) {
-        $(this).parent().append('<div class="dotdotdot-arr"><i class="icon icon_arrow-small-down"></div>');
-    }
+   if ( isTruncated ) {
+      $(this).parent().append('<div class="dotdotdot-arr"><i class="icon icon_arrow-small-down"></div>');
+   }
 });
 
 // open dotdotdot
 
 $('.dotdotdot-arr').on('click', function(e) {
-    e.preventDefault();
-    var t = $(this),
-        outerBlock = t.parent(),
-        dotedBlock = outerBlock.find('.info-cut'),
-        hideBlock = outerBlock.find('.hideble-block');
+   e.preventDefault();
+   var t = $(this),
+       outerBlock = t.parent(),
+       dotedBlock = outerBlock.find('.info-cut'),
+       hideBlock = outerBlock.find('.hideble-block'),
+       play = outerBlock.find('.play-video2');
 
-    t.add(dotedBlock).toggleClass('open');
-    hideBlock.slideToggle();
+   t.add(dotedBlock).toggleClass('open');
+   hideBlock.slideToggle();
+   play.slideToggle();
 
-    dotedBlock.trigger('originalContent', function( content ) {
-        dotedBlock.html( content );
-    });
+   dotedBlock.trigger('originalContent', function( content ) {
+      dotedBlock.html( content );
+   });
 
 });
